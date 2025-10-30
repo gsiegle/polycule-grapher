eb86f4193d2dbdf87b3e7c3761ba5a0a50bdee89
Elipsis <junkgoeshere21@hotmail.com>
2025-10-29 22:02:47 -0400

    Cleanup = true!

--- renderfriend.py
+++ renderfriend.py
@@ -2,15 +2,6 @@ import os
 import graphviz
 from graphviz import Source
 
-def dotCleanup():
-    allFiles = os.listdir('DOT')
-
-    for aFile in allFiles:
-        if (aFile.split(".")[1].lower() == "dot"):
-            print ("Deleting file: " + aFile)
-            os.remove("SVG/" + aFile)
-
-
 def main():
     
     renderList = os.listdir("DOT")
@@ -22,10 +13,10 @@ def main():
         # Render the graph to an SVG file
         # The output file will be named 'my_graph.gv.svg' by default, 
         # or you can specify a different filename in the render() method.
-        dots.render(format='svg', view=False, directory="SVG")
+        dots.render(format='svg', view=False, directory="SVG", cleanup=True)
 
         print(f"SVG generated from '{renderMe}'")
 
-    dotCleanup()
+    #dotCleanup()
 
 main()
